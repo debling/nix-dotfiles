933a1926db60f10e81151d3467c6caddc6945a25
Denilson dos Santos Ebling <d.ebling8@gmail.com>
2024-07-25 22:45:03 -0300

    feat: dwlb status bar

--- modules/desktop/dwl/config.def.h
+++ modules/desktop/dwl/config.def.h
@@ -120,7 +120,7 @@ static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TA
 
 /* commands */
 static const char *termcmd[] = { "alacritty", NULL };
-static const char *menucmd[] = { "rofi -show dru", NULL };
+static const char *menucmd[] = { "rofi", "-show", "drun", NULL };
 
 static const Key keys[] = {
 	/* Note that Shift changes certain key codes: c -> C, 2 -> at, etc. */
--- modules/desktop/dwl/default.nix
+++ modules/desktop/dwl/default.nix
@@ -1,16 +1,32 @@
 { config, lib, pkgs, ... }:
 
 
-let 
-    dwl = pkgs.dwl.override { conf = ./config.def.h; };
-in {
+let
+  dwlb = pkgs.callPackage ./dwlb.nix { };
+  dwl-patches = pkgs.fetchFromGitea {
+    domain = "codeberg.org";
+    owner = "dwl";
+    repo = "dwl-patches";
+    rev = "5252fe728ed5bc62ba70bd2c9b0d36df074d20e9";
+    hash = "sha256-mD4mc6CzJaN+Z+Kz5WHkRNNh7Q/OSWkt6BOZXlCoGj8=";
+  };
+  dwl = pkgs.dwl.override {
+    # patches = [
+    #   "${dwl-patches.outPath}/patches/bar/bar.patch"
+    # ];
+    conf = ./config.def.h;
+  };
+in
+{
   config = {
+    programs.dconf.enable = true;
     xdg.portal = {
       enable = true;
       extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
     };
 
     environment = {
+      systemPackages = [ dwl dwlb ];
       sessionVariables = {
         WLR_NO_HARDWARE_CURSORS = "1";
         # Hint electron apps to use wayland
@@ -23,7 +39,7 @@ in {
       settings =
         let
           tuigreet = lib.getExe pkgs.greetd.tuigreet;
-          session = lib.getExe pkgs.dwl;
+          session = "${lib.getExe dwl} -s '${lib.getExe dwlb} -font \"mono:size=10\"'";
           username = "debling";
         in
         {
