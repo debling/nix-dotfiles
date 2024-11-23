63a8f11f56936c5997b03666b08739aa23081679
Denilson dos Santos Ebling <d.ebling8@gmail.com>
2024-11-23 13:41:41 -0300

    chore(disko): revised mount options

--- disko.nix
+++ disko.nix
@@ -20,6 +20,7 @@
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
               };
             };
             luks = {
@@ -33,11 +34,38 @@
                   type = "filesystem";
                   format = "f2fs";
                   mountpoint = "/";
+                  extraArgs = [
+                    # Extends node bitmap
+                    # this will increaase the number of inodes, to prevent the
+                    # case of the drive have free space, but the disk is full
+                    # bcs theres no inode lefs
+                    # see: https://lore.kernel.org/all/CAF_dkJB%3d2PAqes+41xAi74Z3X0dSjQzCd9eMwDjpKmLD9PBq6A@mail.gmail.com/T/
+                    "-i" 
+
+                    # Flags
+                    "-O"
+                    # - inode_checksum and sb_checksum to help detect
+                    # corruption, both require the extra_attr flag to be
+                    # enabled as well
+                    # - compression, enable transparent compression, also
+                    # requires the extra_attr
+                    "extra_attr,inode_checksum,sb_checksum,compression"
+                  ];
                   mountOptions = [
-                    # "compress_algorithm=zstd:6"
-                    # "compress_chksum"
+                    # tells F2FS to use zstd for compression at level 6, which
+                    # should give pretty good compression ratio.
+                    "compress_algorithm=zstd:6"
+                    # tells the filesystem to verify compressed blocks with a
+                    # checksum (to avoid corruption)
+                    "compress_chksum"
+
+                    # Enable better garbage collector, and enable some
+                    # foreground garbage collections to be asynchronous
                     "atgc"
                     "gc_merge"
+
+                    # Do not synchronously update access or modification times.
+                    # Improves IO performance and flash durability. 
                     "lazytime"
                   ];
                 };
