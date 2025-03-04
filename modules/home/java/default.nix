308e6644b6f1e4b6b7105ea1ebd18ca4191abe3a
Denilson dos Santos Ebling <d.ebling8@gmail.com>
2025-03-04 13:30:00 -0300

    chore: adding javafx to jdks

--- modules/home/java/default.nix
+++ modules/home/java/default.nix
@@ -3,18 +3,19 @@
 with pkgs;
 
 let 
-  latest_jdk = jdk23;
+  withFx = jdk: jdk23.override { enableJavaFX = true;};
+  latest_jdk = withFx jdk23;
   javadoc = callPackage ./javadoc.nix { jdk = latest_jdk; };
 in {
   home = {
-    packages = [ latest_jdk javadoc mvnd ];
+    packages = [ latest_jdk javadoc mvnd scenebuilder ];
     file = { 
       # Stable SDK symlinks
-      "SDKs/Java/23".source = jdk23.home;
-      "SDKs/Java/21".source = jdk21.home;
-      "SDKs/Java/17".source = jdk17.home;
-      "SDKs/Java/11".source = jdk11.home;
-      "SDKs/Java/8".source  = jdk8.home;
+      "SDKs/Java/23".source = (withFx jdk23).home;
+      "SDKs/Java/21".source = (withFx jdk21).home;
+      "SDKs/Java/17".source = (withFx jdk17).home;
+      "SDKs/Java/11".source = (withFx jdk11).home;
+      "SDKs/Java/8".source  = (withFx jdk8).home;
       "SDKs/graalvm".source = graalvmPackages.graalvm-ce.home;
     };
 
