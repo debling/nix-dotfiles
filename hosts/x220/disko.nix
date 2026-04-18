4be193fe5faf6c0d34409251d116fc57d493b20d
Denilson dos Santos Ebling <d.ebling8@gmail.com>
2026-04-17 21:18:42 -0300

    updating x220 drive

--- hosts/x220/disko.nix
+++ hosts/x220/disko.nix
@@ -1,73 +1,88 @@
-{ lib, ... }:
 {
   disko.devices = {
     disk = {
-      main = {
-        # When using disko-install, we will overwrite this value from the commandline
-        device = "/dev/disk/by-id/ata-WDC_WDS120G2G0A-00JH30_182142806025";
+      x220sata0 = {
         type = "disk";
+        device = "/dev/disk/by-id/nvme-WDS250G3X0C-00SJG0_2031B4803308";
         content = {
           type = "gpt";
+
           partitions = {
-            ESP = {
+            esp = {
+              size = "1G";
               type = "EF00";
-              size = "500M";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
+                mountOptions = [
+                  "umask=0077"
+                  "defaults"
+                ];
               };
             };
+
+            swap = {
+              size = "16G";
+              content = {
+                type = "luks";
+                name = "crypt-swap";
+                settings.allowDiscards = true;
+                content.type = "swap";
+              };
+            };
+
             luks = {
               size = "100%";
               content = {
                 type = "luks";
-                name = "crypted-root";
+                name = "crypt-x220sata0";
                 settings.allowDiscards = true;
-                passwordFile = "/tmp/secret.key";
-                content = {
-                  type = "filesystem";
-                  format = "f2fs";
-                  mountpoint = "/";
-                  extraArgs = [
-                    # Extends node bitmap
-                    # this will increaase the number of inodes, to prevent the
-                    # case of the drive have free space, but the disk is full
-                    # bcs theres no inode lefs
-                    # see: https://lore.kernel.org/all/CAF_dkJB%3d2PAqes+41xAi74Z3X0dSjQzCd9eMwDjpKmLD9PBq6A@mail.gmail.com/T/
-                    "-i"
 
-                    # Flags
-                    # - inode_checksum and sb_checksum to help detect
-                    # corruption, both require the extra_attr flag to be
-                    # enabled as well
-                    # - compression, enable transparent compression, also
-                    # requires the extra_attr
-                    "-O"
-                    "extra_attr,inode_checksum,sb_checksum,compression"
-                  ];
-                  mountOptions = [
-                    # "compress_algorithm=zstd:6"
-                    # tells F2FS to use zstd for compression at level 6, which
-                    # should give pretty good compression ratio.
-                    # "compress_chksum"
-                    # tells the filesystem to verify compressed blocks with a
-                    # checksum (to avoid corruption)
-                    # "atgc" "gc_merge"
-                    # Enable better garbage collector, and enable some
-                    # foreground garbage collections to be asynchronous
-                    # "lazytime"
-                    # Do not synchronously update access or modification times.
-                    # Improves IO performance and flash durability.
-                    "compress_algorithm=zstd:6,compress_chksum,atgc,gc_merge,lazytime,nodiscard"
-                  ];
+                content = {
+                  type = "btrfs";
+                  subvolumes = {
+                    "@root" = {
+                      mountpoint = "/";
+                      mountOptions = [
+                        "noatime"
+                        "compress=zstd"
+                      ];
+                    };
+
+                    "@nix" = {
+                      mountpoint = "/nix";
+                      mountOptions = [
+                        "noatime"
+                        "compress=zstd"
+                      ];
+                    };
+
+                    "@var" = {
+                      mountpoint = "/var";
+                      mountOptions = [
+                        "noatime"
+                        "compress=zstd"
+                      ];
+                    };
+
+                      "@samba" = {
+                          mountpoint = "/srv/samba";
+                          mountOptions = [ "compress=zstd" "noatime" ];
+                      };
+
+                      "@media" = {
+                          mountpoint = "/srv/media";
+                          mountOptions = [ "compress=no" "noatime" ];
+                      };
+                    };
+                  };
                 };
               };
             };
           };
         };
       };
+
     };
-  };
 }
