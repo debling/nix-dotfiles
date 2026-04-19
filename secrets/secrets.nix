4a2216baec38ed5fbc25bf54ae4be995a803d7fc
Denilson dos Santos Ebling <d.ebling8@gmail.com>
2026-04-19 02:13:21 -0300

    updated x220 keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFJdyN9ifYpEHZI2jXe7YYKVfNQMuAmofsgg7Txf3YSq d.ebling8@gmail.com"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPUuCteGtKvL12/cAu9GwaLVWjWEtGHh6mTr1cfFwWGV root@x220"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFQKHHm9h+5HuoPCGzcEc11oVl/V1BcITzH/9XeMdAP7 root@x220"
   ];
 in
 {
